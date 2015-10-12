51a92757f7881bf8ce5765bf6c99c5fe69848749
DarkMio <mio@zier.ws>
2015-10-12 20:23:28 +0200

    skeleton done

--- index.php
+++ index.php
@@ -6,8 +6,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>tattoofrei.de</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
+    <link rel="stylesheet" href="./bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="./bootstrap/css/bootstrap-theme.min.css">
     <style>
         body {
         }
@@ -17,8 +17,18 @@
             padding: 40px 15px;
             text-align: center;
         }
+        .hovercraft {
+            max-width: 750px;
+            margin-left: auto;
+            margin-right: auto;
+            padding: 15px;
+            background-color: white;
+        }
+        .jumbotron {
+            background-color: #fafafa;
+        }
     </style>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+    <script src="./bootstrap/js/bootstrap.min.js"></script>
 </head>
 <body>
     <header class="navbar">
@@ -36,21 +46,66 @@
     <div class="container-fluid">
         <div class="row">
             <div class="welcome">
-                <h1>Hier könnte meine Map sein.</h1>
+                <h1>Super coole GMap</h1>
                 <div class="hovercraft">
                     <div class="row">
-
+                        <div class="col-lg-6">
+                            Lorem
+                        </div>
+                        <div class="col-lg-6">
+                            Ipsum
+                        </div>
                     </div>
+                </div>
+            </div>
+            <div class="container">
+                <br />
+                <div class="jumbotron">
+                   <h1 class="center-block">Gefunded Studios</h1>
+                    <br />
+                    <table class="table table-striped table-hover">
+                        <thead>
+                        <tr>
+                            <th>Plz</th>
+                            <th>Name</th>
+                            <th>Hausnummer</th>
+                            <th>Ort</th>
+                            <th>Bewertung</th>
+                        </tr>
+                        </thead>
+                        <tr>
+                            <td>12103</td>
+                            <td>Mio Moto</td>
+                            <td>Blumenthalstr. 15</td>
+                            <td>Berlin-Tempelhof</td>
+                            <td>8/8 m8</td>
+                        </tr>
+                        <tr>
+                            <td>16784</td>
+                            <td>Testdata</td>
+                            <td>67 a</td>
+                            <td>Berlin</td>
+                            <td>4/8</td>
+                        </tr>
+                    </table>
                 </div>
             </div>
         </div>
     </div>
-    <footer>
-        <div class="container">
-            <h3>From Russia with love. This is also a footer.</h3>
-            <span class="label label-success">
-                &pi; <?php echo round((microtime(TRUE)-$_SERVER['REQUEST_TIME_FLOAT']), 4); ?>
-            </span>
+    <footer class="panel-footer">
+        <div class="row">
+            <div class="col-lg-12">
+                <ul class="list-inline">
+                    <li><a href="#">Kontakt</a></li>
+                    <li><a href="#">Werte</a></li>
+                    <li><a href="#">Impressum</a></li>
+                </ul>
+                <p>From Russia with love. This is also a footer.
+                <span class="label label-success">
+                    &pi; <?php echo round((microtime(TRUE)-$_SERVER['REQUEST_TIME_FLOAT']), 4); ?>
+                </span>
+                </p>
+            </div>
         </div>
     </footer>
 </body>

ab17e1eae223249172db0a21484f51ba605e1327
DarkMio <mio@zier.ws>
2015-10-25 01:14:27 +0200

    pattern integration

--- input.php
+++ input.php
@@ -14,7 +14,7 @@
     <script src='https://www.google.com/recaptcha/api.js'></script>
 </head>
 <body>
-<?php include 'nav.php';?>
+<?php include 'nav.php'; ?>
 <div class="container-fluid">
     <div class="row">
         <div class="container">
@@ -22,21 +22,39 @@
                 <fieldset>
                     <legend>Ein neues Studio registrieren:</legend>
                     <div class="form-group">
-                        <label for="Name" class="col-lg-2 control-label">Name</label>
+                        <label for="Name" class="col-lg-2 control-label">Name *</label>
                         <div class="col-lg-10">
                             <input class="form-control" id="Name" placeholder="Studioname" type="text" name="studioName" required>
                         </div>
                     </div>
                     <div class="form-group">
-                        <label for="Ort" class="col-lg-2 control-label">Plz Ort</label>
+                        <label for="Ort" class="col-lg-2 control-label">Plz Ort *</label>
                         <div class="col-lg-10">
-                            <input class="form-control" id="Ort" placeholder="Postleitzahl Ort" type="text" name="studioLocation" required>
+                            <input class="form-control" id="Ort" placeholder="Postleitzahl Ort" type="text" name="studioLocation" pattern="[0-9]{5}\s[\u00C0-\u017Fa-zA-Z'][\u00C0-\u017Fa-zA-Z-' ]+" required>
                         </div>
                     </div>
                     <div class="form-group">
-                        <label for="Straße" class="col-lg-2 control-label">Anschrift</label>
+                        <label for="Straße" class="col-lg-2 control-label">Anschrift *</label>
                         <div class="col-lg-10">
-                            <input class="form-control" id="Straße" placeholder="Stra&szlig;e Hausnummer" type="text" name="studioAddress" required>
+                            <input class="form-control" id="Straße" placeholder="Hausnummer Straße" type="text" name="studioAddress" pattern="[\u00C0-\u017Fa-zA-Z-'\s]+\s[0-9]+[\w]" required>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="Straße" class="col-lg-2 control-label">Telefon</label>
+                        <div class="col-lg-10">
+                            <input class="form-control" id="Telefon" placeholder="Telefonnummer" type="tel" name="studioPhone" pattern="^(?:0|\(?\+33\)?\s?|0033\s?)[1-79](?:[\.\-\s]?\d\d){4}$">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="Straße" class="col-lg-2 control-label">Webseite</label>
+                        <div class="col-lg-10">
+                            <input class="form-control" id="URL" placeholder="URL / Facebook / @Twitter" type="url" name="studioURL">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="E-Mail" class="col-lg-2 control-label">E-Mail</label>
+                        <div class="col-lg-10">
+                            <input class="form-control" id="Mail" placeholder="E-Mail Adresse" type="email" name="studioMail">
                         </div>
                     </div>
                     <div class="form-group">
@@ -44,30 +62,36 @@
                         <div class="col-lg-10">
                             <select class="form-control accordion-dropdown" id="select" name="submitType">
                                 <option value="withAddress studio">Studio Inhaber</option>
-                                <option selected="selected" value="withAddress user">Gast</option>
-                                <option value="withoutAddress">Anonym</option>
+                                <option value="withAddress user">Gast</option>
+                                <option value="withoutAddress" selected="selected">Anonym</option>
                             </select>
                         </div>
                     </div>
                     <div class="accordion" id="accordion">
                         <div class="accordion-group">
-                            <div class="panel-collapse collapse in" id="collapseExample">
+                            <div class="panel-collapse collapse" id="userInfo">
                                 <div class="form-group">
                                     <label for="Name" class="col-lg-2 control-label">Name</label>
                                     <div class="col-lg-10">
-                                        <input class="form-control" id="Name" placeholder="Name / Kürzel" type="text" name="userName">
+                                        <input class="form-control" id="Name" placeholder="Name / Kürzel" type="text" name="userName" pattern="[\u00C0-\u017Fa-zA-Z-']+\w[\u00C0-\u017Fa-zA-Z-']">
                                     </div>
                                 </div>
                                 <div class="form-group">
                                     <label for="Ort" class="col-lg-2 control-label">Plz Ort</label>
                                     <div class="col-lg-10">
-                                        <input class="form-control" id="Ort" placeholder="Postleitzahl Ort" type="text" name="userLocation">
+                                        <input class="form-control" id="Ort" placeholder="Postleitzahl Ort" type="text" name="userLocation" pattern="[0-9]{5}\s[\u00C0-\u017Fa-zA-Z-' ]+">
                                     </div>
                                 </div>
                                 <div class="form-group">
                                     <label for="Straße" class="col-lg-2 control-label">Anschrift</label>
                                     <div class="col-lg-10">
-                                        <input class="form-control" id="Straße" placeholder="Straße Hausnummer" type="text" name="userAddress">
+                                        <input class="form-control" id="Straße" placeholder="Straße Hausnummer" type="text" name="userAddress" pattern="[\u00C0-\u017Fa-zA-Z-'\s]+\s[0-9]+[\w]">
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <label for="E-Mail" class="col-lg-2 control-label">E-Mail</label>
+                                    <div class="col-lg-10">
+                                        <input class="form-control" id="Mail" placeholder="E-Mail Adresse" type="email" name="userMail">
                                     </div>
                                 </div>
                             </div>
@@ -89,24 +113,15 @@
         </div>
     </div>
 </div>
-<?php include 'footer.php';?>
+<?php include 'footer.php'; ?>
 </body>
 <script>
     $( document ).ready(function() {
-
-        $(".alert").find(".close").on("click", function (e) {
-            // Find all elements with the "alert" class, get all descendant elements with the class "close", and bind a "click" event handler
-            e.stopPropagation();    // Don't allow the click to bubble up the DOM
-            e.preventDefault();    // Don't let any default functionality occur (in case it's a link)
-            $(this).closest(".alert").slideUp(400);    // Hide this specific Alert
-        });
-
-
         $("#select").change(function(){
             if ( $(this).val().indexOf("withAddress") != -1){
-                $('#collapseExample').collapse('show');
+                $('#userInfo').collapse('show');
             } else {
-                $('#collapseExample').collapse('hide');
+                $('#userInfo').collapse('hide');
             }
         })
     });
